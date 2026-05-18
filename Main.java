16b7d0394592379c2e9f2f4d743d7126b7b6d6fc
jaretholivas <w7392247@student.miracosta.edu>
2026-05-18 22:15:54 +0000

    Completed Lab 1

--- Main.java
+++ Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Welcome To CS 112!");
+        AboutMe();
+        Person myPerson = new Person("Jareth", 20);
+        AgeInDays(20);
+        System.out.println(myPerson);
+    }
+    
+    public static void AboutMe()
+    {
+        System.out.println("My name is Jareth and I am a student at Miracosta College.");
+    }
+
+    public static void AgeInDays(int age)
+    {
+        System.out.println("Age in days: " + age * 365);
     }
 }
